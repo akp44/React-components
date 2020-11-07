3bc58a55008c3a0885152741df7afc0bbf2dd40c
akshaypandey44 <kumarakshaypandey@gmail.com>
2020-11-07 18:39:39 +0530

    final

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ class Sample extends React.Component {
   render() {
     return <p>I am learning React. My life is getting better.</p>;
   }
-}
+} 
 
 ReactDOM.render(<Sample />, document.getElementById('root'));
